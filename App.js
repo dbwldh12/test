28666abdbeb950ab9b531b1414dc2ea82e88b38a
dbwldh12 <165783363+dbwldh12@users.noreply.github.com>
2024-05-13 18:50:34 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,34 @@
+import "./App.css"
+function App() {
+  return(
+    <div>
+      <div className='main_container'>
+        <div className='left_container'>
+          <div className='left_container__1'></div>
+          <div className='left_container__2'>
+            <div className='left_container__2__1'></div>
+          </div>
+          <div className='left_container__3'>
+            <div className='left_container__3__1'></div>
+            <div className='left_container3__2'></div>
+          </div>
+        </div>
+        <div className='mid_container'>
+          <div className='mid_container__1'></div>
+          <div className='mid_container__2'></div>
+          <div className='mid_container__3'></div>
+          <div className='mid_container__3__1'></div>
+          <div className='mid_container__3__2'></div>
+        </div>
+        <div className='right_container'>
+          <div className='right_container__1'>
+            <div className='right_container__1__1'></div>
+            <div className='right_container__1__2'></div>
+            <div className='right_container__1__3'></div>
+          </div>
+          <div className='right_container__2'></div>
+        </div>
+      </div>
+    </div>
+  );
+};
